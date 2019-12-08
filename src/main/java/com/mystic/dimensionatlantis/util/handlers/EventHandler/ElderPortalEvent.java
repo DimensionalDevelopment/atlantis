8a53a005ad76b93e247b8b82518ea9609d97512f
Mysticpasta1 <a5ethancantrell@gmail.com>
2019-12-07 18:37:31 -0600

    add @subscriberevent

--- src/main/java/com/mystic/dimensionatlantis/util/handlers/EventHandler/ElderPortalEvent.java
+++ src/main/java/com/mystic/dimensionatlantis/util/handlers/EventHandler/ElderPortalEvent.java
@@ -8,11 +8,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber
 public class ElderPortalEvent {
 
 	public static net.minecraft.world.gen.structure.MapGenStructure field_75053_d ,structureMap;
+	@SubscribeEvent
     public static void onDeath(LivingDeathEvent event) {
     	
     	Entity Entity = event.getEntity();
