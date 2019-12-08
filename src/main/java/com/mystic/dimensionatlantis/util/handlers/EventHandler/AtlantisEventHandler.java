5e340add9ef52e2bd571bf070af2b096cf772ebe
Mysticpasta1 <a5ethancantrell@gmail.com>
2019-12-07 18:30:50 -0600

    moved LivingDeathEvent

--- src/main/java/com/mystic/dimensionatlantis/util/handlers/EventHandler/AtlantisEventHandler.java
+++ src/main/java/com/mystic/dimensionatlantis/util/handlers/EventHandler/AtlantisEventHandler.java
@@ -9,17 +9,11 @@ import com.mystic.dimensionatlantis.util.Reference;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.entity.monster.EntityElderGuardian;
-import net.minecraft.entity.monster.EntityGuardian;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.gen.structure.StructureStart;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -62,17 +56,4 @@ public class AtlantisEventHandler {
 
     }
     
-    public static net.minecraft.world.gen.structure.MapGenStructure field_75053_d ,structureMap;
-    public static void onDeath(LivingDeathEvent event) {
-    	
-    	if (event.getEntity() instanceof EntityElderGuardian && event.getEntity().world.getEntitiesWithinAABB(EntityElderGuardian.class, event.getEntity().getEntityBoundingBox().expand(100, 100, 100)).isEmpty()){
-    		
-    			System.out.println("entity is died");
-    			World world = event.getEntity().world;
-    		    BlockPos pos = event.getEntity().getPosition();
-    			world.getBlockState(pos).getBlock();
-    			world.setBlockState(pos, AtlantisPortal.PORTAL.getDefaultState());
-    		
-    	}
     }
-}
---
+++ src/main/java/com/mystic/dimensionatlantis/util/handlers/EventHandler/ElderPortalEvent.java
@@ -0,0 +1,39 @@
+package com.mystic.dimensionatlantis.util.handlers.EventHandler;
+
+import com.mystic.dimensionatlantis.blocks.AtlantisPortal;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityElderGuardian;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ElderPortalEvent {
+
+	public static net.minecraft.world.gen.structure.MapGenStructure field_75053_d ,structureMap;
+    public static void onDeath(LivingDeathEvent event) {
+    	
+    	Entity Entity = event.getEntity();
+    	boolean Death = Entity.isDead;
+    	
+    	if (Entity instanceof EntityElderGuardian && event.getEntity()
+    			.world
+    			.getEntitiesWithinAABB(EntityElderGuardian
+    			.class,
+    			Entity.getEntityBoundingBox()
+    			.expand(100, 100, 100))
+    			.stream()
+    		    .filter(entity -> !Death && entity != event.getEntity())
+    		    .count() == 0){
+    		
+    			System.out.println("entity is died");
+    			World world = event.getEntity().world;
+    		    BlockPos pos = event.getEntity().getPosition();
+    			world.getBlockState(pos).getBlock();
+    			world.setBlockState(pos, AtlantisPortal.PORTAL.getDefaultState());
+    		
+    	}
+    }
+}
