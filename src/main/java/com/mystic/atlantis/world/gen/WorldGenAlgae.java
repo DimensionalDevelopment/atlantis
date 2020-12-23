0db25880380578f1f2ec730ed5da26ef5b591e7e
Mysticpasta1 <45608372+Mysticpasta1@users.noreply.github.com>
2020-12-23 05:45:48 -0600

    Made some algae generation

---
+++ src/main/java/com/mystic/atlantis/world/gen/WorldGenAlgae.java
@@ -0,0 +1,64 @@
+package com.mystic.atlantis.world.gen;
+
+import com.mystic.atlantis.config.AtlantisConfig;
+import com.mystic.atlantis.world.biomes.BiomeATLANTIS;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class WorldGenAlgae implements IWorldGenerator
+{
+    public final WorldGeneratorAlgae ALGAE = new WorldGeneratorAlgae();
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
+    {
+        if(world.provider.getDimension() == AtlantisConfig.dimensionId)
+        {
+            generatePlant(ALGAE, world, random, chunkX, chunkZ, 10);
+        }
+    }
+
+    private void generatePlant(WorldGeneratorAlgae generator, World world, Random random, int chunkX, int chunkZ, int chance)
+    {
+        int x = (chunkX * 16) + random.nextInt(15) + 8;
+        int z = (chunkZ * 16) + random.nextInt(15) + 8;
+        int y = calculateGenerationHeight(world, x, z);
+        BlockPos pos = new BlockPos(x,y,z);
+
+        world.provider.getBiomeForCoords(pos);
+        BiomeATLANTIS.getBiome(10);
+
+
+        if(world.getWorldType() != WorldType.FLAT)
+        {
+            if(BiomeDictionary.hasType(Objects.requireNonNull(BiomeATLANTIS.getBiome(10)), BiomeDictionary.Type.OCEAN))
+            {
+                if(random.nextInt(chance) == 0)
+                {
+                    generator.generate(world, random, pos);
+                }
+            }
+        }
+    }
+
+    private int calculateGenerationHeight(World world, int x, int z)
+    {
+        for (int y=0; y<300; y++)
+        {
+            if (world.getBlockState(new BlockPos(x, y, z)).getBlock() == Blocks.WATER)
+            {
+                return y;
+            }
+        }
+        return 3000;
+    }
+}
---
+++ src/main/java/com/mystic/atlantis/world/gen/WorldGeneratorAlgae.java
@@ -0,0 +1,47 @@
+package com.mystic.atlantis.world.gen;
+
+import com.mystic.atlantis.init.ModBiomes;
+import com.mystic.atlantis.init.ModBlocks;
+import io.github.opencubicchunks.cubicchunks.core.CubicChunks;
+import net.minecraft.block.BlockVine;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+import java.util.Random;
+
+public class WorldGeneratorAlgae extends WorldGenerator
+{
+
+    public boolean generate(World worldIn, Random rand, BlockPos position) {
+        for (; position.getY() < CubicChunks.MAX_SUPPORTED_BLOCK_Y; position = position.up()) {
+            if (isWaterBlock(worldIn, position)) {
+                for (EnumFacing enumfacing : EnumFacing.Plane.HORIZONTAL.facings()) {
+                    if (ModBlocks.ALGAE.canPlaceBlockOnSide(worldIn, position, enumfacing)) {
+                        IBlockState iblockstate = ModBlocks.ALGAE.getDefaultState().withProperty(BlockVine.NORTH, enumfacing == EnumFacing.NORTH).withProperty(BlockVine.EAST, enumfacing == EnumFacing.EAST).withProperty(BlockVine.SOUTH, enumfacing == EnumFacing.SOUTH).withProperty(BlockVine.WEST, enumfacing == EnumFacing.WEST);
+                        worldIn.setBlockState(position, iblockstate, 2);
+                        break;
+                    }
+                }
+            } else {
+                position = position.add(rand.nextInt(4) - rand.nextInt(4), 0, rand.nextInt(4) - rand.nextInt(4));
+            }
+        }
+        return true;
+    }
+
+    public boolean isWaterBlock(World worldIn, BlockPos pos)
+    {
+        return isWater(worldIn.getBlockState(pos), worldIn, pos);
+    }
+
+    public boolean isWater(IBlockState state, IBlockAccess world, BlockPos pos)
+    {
+        return state.getMaterial() == Material.WATER;
+    }
+}
