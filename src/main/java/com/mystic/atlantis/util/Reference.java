bccd268215ab22722c0c502aa34c9fa0fc424913
HypherionMC <hypherionmc@gmail.com>
2020-12-27 03:11:02 +0200

    Jenkins Configuration

--- src/main/java/com/mystic/atlantis/util/Reference.java
+++ src/main/java/com/mystic/atlantis/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MODID = "atlantis";
     public static final String NAME = "Atlantis";
-    public static final String VERSION = "2020.12.24-1.12.2-2.3.9";
+    public static final String VERSION = "@VERSION@";
     public static final String ACCEPTED_VERSION = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.mystic.atlantis.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.mystic.atlantis.proxy.CommonProxy";
