e8ad8e6dc27c7440a7df941a44f6bf759eb2442c
Mysticpasta1 <45608372+Mysticpasta1@users.noreply.github.com>
2021-01-07 01:03:43 -0600

    finished making loot tables ore blocks :)

--- src/main/java/com/mystic/atlantis/blocks/AquamarineOre.java
+++ src/main/java/com/mystic/atlantis/blocks/AquamarineOre.java
@@ -1,8 +1,18 @@
 package com.mystic.atlantis.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
+
+import javax.annotation.Nullable;
 
 public class AquamarineOre extends Block
 {
