8dd825f574b29ac45486c27cc1d066b9cc235be3
Mysticpasta1 <45608372+Mysticpasta1@users.noreply.github.com>
2021-01-09 01:42:09 -0600

    version changes

--- src/main/java/com/mystic/atlantis/util/Reference.java
+++ src/main/java/com/mystic/atlantis/util/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 
     public static final String MODID = "atlantis";
     public static final String NAME = "Atlantis";
-    public static final String VERSION = "2020.12.26-1.16.4-1.0.0-forge";
+    public static final String VERSION = "2020.1.7-1.16.4-1.1.0-forge";
     public static final String ACCEPTED_VERSION = "[1.16.4, 1.17)";
 
 }
