456346f292e394bb5acec17af850d3e1901415cd
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-03-19 10:28:13 -0500

    fixed ore generation

--- src/main/java/com/mystic/atlantis/Main.java
+++ src/main/java/com/mystic/atlantis/Main.java
@@ -5,14 +5,25 @@ import java.util.Map;
 
 import com.mystic.atlantis.blocks.blockentities.DummyDataStorage;
 import com.mystic.atlantis.init.BlockInit;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.chunk.FlatChunkGenerator;
 import net.minecraft.world.gen.chunk.StructureConfig;
 import net.minecraft.world.gen.chunk.StructuresConfig;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
 
 import com.mystic.atlantis.configfeature.AtlantisFeature;
@@ -29,6 +40,18 @@ import net.fabricmc.fabric.api.event.player.UseItemCallback;
 
 public class Main implements ModInitializer
 {
+
+    private static final ConfiguredFeature<?, ?> ORE_AQUAMARINE_OVERWORLD = Feature.ORE
+            .configure(new OreFeatureConfig(
+                    OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+                    BlockInit.AQUAMARINE_ORE.getDefaultState(),
+                    9)) // vein size
+            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
+                    0,
+                    0,
+                    64)))
+            .spreadHorizontally()
+            .repeat(20); // number of veins per chunk
 
     public void addDimensionalSpacing(MinecraftServer server, ServerWorld world) {
         if(world != null){
@@ -53,6 +76,11 @@ public class Main implements ModInitializer
         AtlantisFeature.ConfiguredFeaturesAtlantis.registerConfiguredFeatures();
         AtlantisStructures.setupAndRegisterStructureFeatures();
         AtlantisConfiguredStructures.registerConfiguredStructures();
+
+        RegistryKey<ConfiguredFeature<?, ?>> oreAquamarineOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
+                new Identifier("atlantis", "ore_aquamarine_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreAquamarineOverworld.getValue(), ORE_AQUAMARINE_OVERWORLD);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreAquamarineOverworld);
 
         ServerWorldEvents.LOAD.register(this::addDimensionalSpacing);
 
--- src/main/java/com/mystic/atlantis/configfeature/AtlantisFeature.java
+++ src/main/java/com/mystic/atlantis/configfeature/AtlantisFeature.java
@@ -1,5 +1,9 @@
 package com.mystic.atlantis.configfeature;
 
+import com.mystic.atlantis.util.Reference;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
@@ -11,13 +15,6 @@ import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-
-import com.mystic.atlantis.init.BlockInit;
-import com.mystic.atlantis.util.Reference;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 
 public class AtlantisFeature {
     public static final Feature<CountConfig> UNDERWATER_FLOWER_ATLANTIS = register(
@@ -26,8 +23,6 @@ public class AtlantisFeature {
             "algae_feature_atlantis", new AlgaeFeatureAtlantis(DefaultFeatureConfig.CODEC));
     public static final Feature<CountConfig> SHELL_BLOCK_FEATURE = register(
             "shell_block_feature", new ShellBlockFeature(CountConfig.CODEC));
-    public static final Feature<AtlantisOreFeatureConfig> AQUAMARINE_ORE_FEATURE = register(
-            "aquamarine_ore_feature", new AquamarineOreFeature(AtlantisOreFeatureConfig.CODEC));
 
     public static <T extends FeatureConfig> Feature<T> register(String id, Feature<T> t) {
         Registry.register(Registry.FEATURE, new Identifier(Reference.MODID, id), t);
@@ -40,12 +35,10 @@ public class AtlantisFeature {
         public static final ConfiguredFeature<?, ?> UNDERWATER_FLOWER_ATLANTIS = AtlantisFeature.UNDERWATER_FLOWER_ATLANTIS.configure(new CountConfig(UniformIntDistribution.of(l)));
         public static final ConfiguredFeature<?, ?> ALGAE_FEATURE_ATLANTIS = AtlantisFeature.ALGAE_FEATURE_ATLANTIS.configure(FeatureConfig.DEFAULT);
         public static final ConfiguredFeature<?, ?> SHELL_BLOCK_FEATURE = AtlantisFeature.SHELL_BLOCK_FEATURE.configure(new CountConfig(UniformIntDistribution.of(l)));
-        public static final ConfiguredFeature<?, ?> AQUAMARINE_ORE_FEATURE = AtlantisFeature.AQUAMARINE_ORE_FEATURE.configure(new AtlantisOreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockInit.AQUAMARINE_ORE.getDefaultState(), 30, "Aquamarine_Ore_Feature_Configuration"));
 
         public static final RegistryKey<ConfiguredFeature<?,?>> UNDERWATER_FLOWER_ATLANTIS_KEY = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(Reference.MODID, "underwater_flower_altantis"));
         public static final RegistryKey<ConfiguredFeature<?,?>> ALGAE_FEATURE_ATLANTIS_KEY = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(Reference.MODID, "algae_feature_altantis"));
         public static final RegistryKey<ConfiguredFeature<?,?>> SHELL_BLOCK_FEATURE_KEY = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(Reference.MODID, "shell_block_feature"));
-        public static final RegistryKey<ConfiguredFeature<?,?>> AQUAMARINE_ORE_FEATURE_KEY = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(Reference.MODID, "aquamarine_ore_feature"));
 
         public static void registerConfiguredFeatures() {
             
@@ -55,15 +48,12 @@ public class AtlantisFeature {
 
             register("shell_block_feature", ConfiguredFeaturesAtlantis.SHELL_BLOCK_FEATURE.rangeOf(32).spreadHorizontally().repeat(100));
 
-            register("aquamarine_ore_feature", ConfiguredFeaturesAtlantis.AQUAMARINE_ORE_FEATURE.rangeOf(15).spreadHorizontally().repeat(30));
-
             BiomeModifications.create(new Identifier(Reference.MODID, "feature_removal")).add(ModificationPhase.REMOVALS,
                     BiomeSelectors.foundInTheEnd().or(BiomeSelectors.foundInTheNether()),
                     biomeModificationContext -> {
                 biomeModificationContext.getGenerationSettings().removeFeature(GenerationStep.Feature.VEGETAL_DECORATION, UNDERWATER_FLOWER_ATLANTIS_KEY);
                 biomeModificationContext.getGenerationSettings().removeFeature(GenerationStep.Feature.VEGETAL_DECORATION, ALGAE_FEATURE_ATLANTIS_KEY);
                 biomeModificationContext.getGenerationSettings().removeFeature(GenerationStep.Feature.SURFACE_STRUCTURES, SHELL_BLOCK_FEATURE_KEY);
-                biomeModificationContext.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, AQUAMARINE_ORE_FEATURE_KEY);
             });
         }
 
