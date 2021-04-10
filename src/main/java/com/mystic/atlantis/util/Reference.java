2ca382de16e308d7f4c1c8e0a3e92524e72e6d8f
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-04-10 00:11:15 -0500

    temporarily removed structure until  https://bugs.mojang.com/browse/MC-197616 is fixed

--- src/main/java/com/mystic/atlantis/util/Reference.java
+++ src/main/java/com/mystic/atlantis/util/Reference.java
@@ -3,7 +3,7 @@ package com.mystic.atlantis.util;
 public class Reference {
     public static final String MODID = "atlantis";
     public static final String NAME = "Atlantis";
-    public static final String VERSION = "2021.4.8-1.17-1.0.0-forge";
+    public static final String VERSION = "2021.4.10-1.17-1.0.0-forge";
     public static final String ACCEPTED_VERSION = "[1.17)";
 
 }
