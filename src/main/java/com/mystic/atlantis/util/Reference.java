7bf49a520ea40880e52705e0f619d3cac016d6a3
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-06-21 11:44:22 -0500

    bumb version

--- src/main/java/com/mystic/atlantis/util/Reference.java
+++ src/main/java/com/mystic/atlantis/util/Reference.java
@@ -3,6 +3,6 @@ package com.mystic.atlantis.util;
 public class Reference {
     public static final String MODID = "atlantis";
     public static final String NAME = "Atlantis";
-    public static final String VERSION = "2021.6.18-1.17-1.3.0-fabric";
+    public static final String VERSION = "2021.6.21-1.17-1.4.0-fabric";
     @Deprecated public static final String ACCEPTED_VERSION = "[1.17)";
 }
