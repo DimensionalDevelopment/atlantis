383df31746467f33d89ec9bbe3770e427d16be62
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-06-20 19:46:56 -0500

    redstone fuckery

--- src/main/java/com/mystic/atlantis/blocks/power/AtlanteanPowerDust.java
+++ src/main/java/com/mystic/atlantis/blocks/power/AtlanteanPowerDust.java
@@ -1,21 +1,130 @@
 package com.mystic.atlantis.blocks.power;
 
+import com.google.common.collect.Sets;
 import com.mystic.atlantis.blocks.plants.UnderwaterFlower;
+import com.mystic.atlantis.mixin.RedstoneAccessor;
 import net.minecraft.block.*;
 import net.minecraft.block.enums.WireConnection;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Property;
-import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.Set;
 
 public class AtlanteanPowerDust extends RedstoneWireBlock implements Waterloggable {
 
     public static final Property<Boolean> WATERLOGGED = UnderwaterFlower.WATERLOGGED;
+
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        if (((RedstoneAccessor) this).getWiresGivePower() && direction != Direction.DOWN) {
+            int i = state.get(POWER);
+            if (i == 0) {
+                return 0;
+            } else {
+                return direction != Direction.UP && !((WireConnection)this.getPlacementState(world, state, pos).get((Property)DIRECTION_TO_WIRE_CONNECTION_PROPERTY.get(direction.getOpposite()))).isConnected() ? 0 : i;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return !((RedstoneAccessor) this).getWiresGivePower() ? 0 : state.getWeakRedstonePower(world, pos, direction);
+    }
+
+    protected static boolean connectsTo(BlockState state) {
+        return connectsTo(state, null);
+    }
+
+    protected static boolean connectsTo(BlockState state, @Nullable Direction dir) {
+        if (state.isOf(Blocks.REDSTONE_WIRE)) {
+            return true;
+        } else if (state.isOf(Blocks.REPEATER)) {
+            Direction direction = state.get(RepeaterBlock.FACING);
+            return direction == dir || direction.getOpposite() == dir;
+        } else if (state.isOf(Blocks.OBSERVER)) {
+            return dir == state.get(ObserverBlock.FACING);
+        } else {
+            return state.emitsRedstonePower() && dir != null;
+        }
+    }
+
+    @Override
+    protected void update(World world, BlockPos pos, BlockState state) {
+        int i = this.getReceivedRedstonePower(world, pos);
+        if (state.get(POWER) != i) {
+            if (world.getBlockState(pos) == state) {
+                world.setBlockState(pos, state.with(POWER, i), Block.NOTIFY_LISTENERS);
+            }
+
+            Set<BlockPos> set = Sets.newHashSet();
+            set.add(pos);
+            Direction[] var6 = Direction.values();
+            int var7 = var6.length;
+
+            for(int var8 = 0; var8 < var7; ++var8) {
+                Direction direction = var6[var8];
+                set.add(pos.offset(direction));
+            }
+
+            Iterator var10 = set.iterator();
+
+            while(var10.hasNext()) {
+                BlockPos blockPos = (BlockPos)var10.next();
+                world.updateNeighborsAlways(blockPos, this);
+            }
+        }
+    }
+
+    @Override
+    public int getReceivedRedstonePower(World world, BlockPos pos) {
+        ((RedstoneAccessor) this).setWiresGivePower(false);
+        int i = world.getReceivedRedstonePower(pos);
+        ((RedstoneAccessor) this).setWiresGivePower(true);
+        int j = 0;
+        if (i < 15) {
+            Iterator var5 = Direction.Type.HORIZONTAL.iterator();
+
+            while(true) {
+                while(var5.hasNext()) {
+                    Direction direction = (Direction)var5.next();
+                    BlockPos blockPos = pos.offset(direction);
+                    BlockState blockState = world.getBlockState(blockPos);
+                    j = Math.max(j, this.increasePower(blockState));
+                    BlockPos blockPos2 = pos.up();
+                    if (blockState.isSolidBlock(world, blockPos) && !world.getBlockState(blockPos2).isSolidBlock(world, blockPos2)) {
+                        j = Math.max(j, this.increasePower(world.getBlockState(blockPos.up())));
+                    } else if (!blockState.isSolidBlock(world, blockPos)) {
+                        j = Math.max(j, this.increasePower(world.getBlockState(blockPos.down())));
+                    }
+                }
+
+                return Math.max(i, j - 1);
+            }
+        } else {
+            return Math.max(i, j - 1);
+        }
+    }
+
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return ((RedstoneAccessor) this).getWiresGivePower();
+    }
+
+    private int increasePower(BlockState state) {
+        return state.isOf(this) ? state.get(POWER) : 0;
+    }
+
     public AtlanteanPowerDust(Settings settings) {
         super(settings.noCollision().breakInstantly());
         this.setDefaultState(getDefaultState().with(WIRE_CONNECTION_NORTH, WireConnection.NONE).with(WIRE_CONNECTION_EAST, WireConnection.NONE).with(WIRE_CONNECTION_SOUTH, WireConnection.NONE).with(WIRE_CONNECTION_WEST, WireConnection.NONE).with(POWER, 0).with(WATERLOGGED, Boolean.TRUE));
@@ -28,13 +137,13 @@ public class AtlanteanPowerDust extends RedstoneWireBlock implements Waterloggab
 
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        if (world.getFluidState(pos).isIn(FluidTags.WATER)) {
+        //if (world.getFluidState(pos).isIn(FluidTags.WATER)) {
             BlockPos blockPos = pos.down();
             BlockState blockState = world.getBlockState(blockPos);
             return this.canRunOnTop(world, blockPos, blockState);
-        } else {
+        /*} else {
             return false;
-        }
+        }*/
     }
 
     private boolean canRunOnTop(BlockView world, BlockPos pos, BlockState floor) {
--- src/main/java/com/mystic/atlantis/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/com/mystic/atlantis/mixin/RedstoneWireBlockMixin.java
@@ -1,20 +1,90 @@
 package com.mystic.atlantis.mixin;
 
+import com.mystic.atlantis.blocks.power.AtlanteanPowerDust;
 import com.mystic.atlantis.init.BlockInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.block.enums.WireConnection;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RedstoneWireBlock.class)
-public abstract class RedstoneWireBlockMixin {
+public abstract class RedstoneWireBlockMixin{
 
+    private static final EnumProperty<WireConnection> WIRE_CONNECTION_NORTH = RedstoneWireBlock.WIRE_CONNECTION_NORTH;
+    private static final EnumProperty<WireConnection> WIRE_CONNECTION_SOUTH = RedstoneWireBlock.WIRE_CONNECTION_SOUTH;
+    private static final EnumProperty<WireConnection> WIRE_CONNECTION_EAST = RedstoneWireBlock.WIRE_CONNECTION_EAST;
+    private static final EnumProperty<WireConnection> WIRE_CONNECTION_WEST = RedstoneWireBlock.WIRE_CONNECTION_WEST;
 
+    @Shadow
+    private static boolean isFullyConnected(BlockState state) {
+        return state.get(WIRE_CONNECTION_NORTH).isConnected() && state.get(WIRE_CONNECTION_SOUTH).isConnected() && state.get(WIRE_CONNECTION_EAST).isConnected() && state.get(WIRE_CONNECTION_WEST).isConnected();
+    }
 
     @Redirect(method = "connectsTo(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", ordinal = 0))
     private static boolean isOf(BlockState state, Block block){
         return state.isOf(block) || state.isOf(BlockInit.ATLANTEAN_POWER_DUST_WIRE);
+    }
+
+    @Inject(method = "getReceivedRedstonePower", at = @At(value = "FIELD", target = "Lnet/minecraft/block/RedstoneWireBlock;wiresGivePower:Z", ordinal = 0))
+    private void setPowerToWires1(World world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        if ((Object) this instanceof AtlanteanPowerDust){
+            ((RedstoneAccessor) Blocks.REDSTONE_WIRE).setWiresGivePower(false);
+        } else {
+            ((RedstoneAccessor) Blocks.REDSTONE_WIRE).setWiresGivePower(false);
+        }
+    }
+
+    @Inject(method = "getReceivedRedstonePower", at = @At(value = "FIELD", target = "Lnet/minecraft/block/RedstoneWireBlock;wiresGivePower:Z", ordinal = 1))
+    private void setPowerToWires2(World world, BlockPos pos, CallbackInfoReturnable<Integer> cir){
+        if ((Object) this instanceof AtlanteanPowerDust) {
+            ((RedstoneAccessor) Blocks.REDSTONE_WIRE).setWiresGivePower(true);
+        } else {
+            ((RedstoneAccessor) Blocks.REDSTONE_WIRE).setWiresGivePower(true);
+        }
+    }
+
+    @Inject(method = "getStrongRedstonePower", at = @At("HEAD"), cancellable = true)
+    private void getPowerToWires1(BlockState state, BlockView world, BlockPos pos, Direction direction, CallbackInfoReturnable<Integer> cir){
+        cir.cancel();
+        if ((Object) this instanceof AtlanteanPowerDust) {
+            if (((RedstoneAccessor) Blocks.REDSTONE_WIRE).getWiresGivePower() && isFullyConnected(state))
+                cir.setReturnValue(0);
+            else cir.setReturnValue(state.getWeakRedstonePower(world, pos, direction));
+        } else {
+            if (((RedstoneAccessor) Blocks.REDSTONE_WIRE).getWiresGivePower() && isFullyConnected(state))
+                cir.setReturnValue(0);
+            else cir.setReturnValue(state.getWeakRedstonePower(world, pos, direction));
+        }
+    }
+    @Redirect(method = "getWeakRedstonePower", at = @At(value = "FIELD", target = "Lnet/minecraft/block/RedstoneWireBlock;wiresGivePower:Z", opcode = Opcodes.GETFIELD))
+    public boolean getPowerToWires2(RedstoneWireBlock redstoneWireBlock) {
+        if ((Object) this instanceof AtlanteanPowerDust) {
+            return ((RedstoneAccessor) Blocks.REDSTONE_WIRE).getWiresGivePower();
+        } else {
+            return ((RedstoneAccessor) Blocks.REDSTONE_WIRE).getWiresGivePower();
+        }
+    }
+
+    @Inject(method = "emitsRedstonePower", at = @At(value = "HEAD"), cancellable = true)
+    public void emitsRedstonePower(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        cir.cancel();
+        if ((Object) this instanceof AtlanteanPowerDust) {
+            cir.setReturnValue(((RedstoneAccessor) Blocks.REDSTONE_WIRE).getWiresGivePower());
+        } else {
+            cir.setReturnValue(((RedstoneAccessor) Blocks.REDSTONE_WIRE).getWiresGivePower());
+        }
     }
 }
