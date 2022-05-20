7cbd6106a255a0eb1cef1ce2eaad4cd7c3d3e486
Mysticpasta1 <ethan.cantrell@asub.edu>
2022-05-20 12:26:08 -0500

    Update LinguisticSymbol.java

--- src/main/java/com/mystic/atlantis/blocks/LinguisticSymbol.java
+++ src/main/java/com/mystic/atlantis/blocks/LinguisticSymbol.java
@@ -1,7 +1,7 @@
 package com.mystic.atlantis.blocks;
 
 public enum LinguisticSymbol {
-    BLANK,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE;
+    BLANK,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE;
 
     @Override
     public String toString() {
