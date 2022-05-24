b51cdc0fdbcdc6bbb76285d7d3edb7469657f153
Waterpicker <waterpickerenternity@gmail.com>
2022-05-24 02:48:40 -0500

    derp

--- src/main/java/com/mystic/atlantis/structures/AtlantisConfiguredStructures.java
+++ src/main/java/com/mystic/atlantis/structures/AtlantisConfiguredStructures.java
@@ -10,11 +10,11 @@ import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfigura
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class AtlantisConfiguredStructures {
-    public static ConfiguredStructureFeature<?, ?> CONFIGURED_OYSTER_STRUCTURE = AtlantisStructures.OYSTER_STRUCTURE.get()..configured(new JigsawConfiguration(() -> PlainVillagePools.START, 0));
-    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_FOUNTAIN = AtlantisStructures.ATLANTEAN_FOUNTAIN.get().configured(new JigsawConfiguration(() -> PlainVillagePools.START, 0));
-    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_HOUSE_1 = AtlantisStructures.ATLANTEAN_HOUSE_1.get().configured(new JigsawConfiguration(() -> PlainVillagePools.START, 0));
-    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_HOUSE_3 = AtlantisStructures.ATLANTEAN_HOUSE_3.get().configured(new JigsawConfiguration(() -> PlainVillagePools.START, 0));
-    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_TOWER = AtlantisStructures.ATLANTEAN_TOWER.get().configured(new JigsawConfiguration(() -> PlainVillagePools.START, 0));
+    public static ConfiguredStructureFeature<?, ?> CONFIGURED_OYSTER_STRUCTURE = AtlantisStructures.OYSTER_STRUCTURE.get().configured(new JigsawConfiguration(PlainVillagePools.START, 0));
+    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_FOUNTAIN = AtlantisStructures.ATLANTEAN_FOUNTAIN.get().configured(new JigsawConfiguration(PlainVillagePools.START, 0));
+    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_HOUSE_1 = AtlantisStructures.ATLANTEAN_HOUSE_1.get().configured(new JigsawConfiguration(PlainVillagePools.START, 0));
+    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_HOUSE_3 = AtlantisStructures.ATLANTEAN_HOUSE_3.get().configured(new JigsawConfiguration(PlainVillagePools.START, 0));
+    public static ConfiguredStructureFeature<?, ?> CONFIGURED_ATLANTEAN_TOWER = AtlantisStructures.ATLANTEAN_TOWER.get().configured(new JigsawConfiguration(PlainVillagePools.START, 0));
 
     public static void registerConfiguredStructures() {
         Registry<ConfiguredStructureFeature<?, ?>> registry = BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE;
