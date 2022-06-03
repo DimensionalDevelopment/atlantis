27dafe6de385b7b3d2b7d3fd115b2c2f5253d5e8
Mysticpasta1 <ethan.cantrell@asub.edu>
2022-06-03 07:11:14 -0500

    Create ThreadHelper.java

---
+++ src/main/java/com/mystic/atlantis/util/ThreadHelper.java
@@ -0,0 +1,10 @@
+package com.mystic.atlantis.util;
+
+public class ThreadHelper {
+    public Thread getThreadByName(String threadName) {
+        for (Thread t : Thread.getAllStackTraces().keySet()) {
+            if (t.getName().equals(threadName)) return t;
+        }
+        return null;
+    }
+}
