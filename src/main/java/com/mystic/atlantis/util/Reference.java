d3d6db18aca65b7ca203616dc4bf4479d94ea40c
Mysticpasta1 <ethana5tc@outlook.com>
2022-08-31 20:37:46 -0500

    fix dependency and updated versioning

--- src/main/java/com/mystic/atlantis/util/Reference.java
+++ src/main/java/com/mystic/atlantis/util/Reference.java
@@ -3,6 +3,6 @@ package com.mystic.atlantis.util;
 public class Reference {
     public static final String MODID = "atlantis";
     public static final String NAME = "Atlantis";
-    public static final String VERSION = "2022.8.26-1.19.2-1.0.0-forge";
+    public static final String VERSION = "2022.8.31-1.19.2-1.0.1-forge";
     @Deprecated public static final String ACCEPTED_VERSION = "[1.19.2, 1.20)";
 }
