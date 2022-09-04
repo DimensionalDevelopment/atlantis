a0529c022766a3483fb92270d1cbe7c1742d0ad6
Mysticpasta1 <ethana5tc@outlook.com>
2022-09-03 22:13:48 -0500

    Advancement fix and temple

--- src/main/java/com/mystic/atlantis/structures/AtlantisStructures.java
+++ src/main/java/com/mystic/atlantis/structures/AtlantisStructures.java
@@ -14,5 +14,6 @@ public class AtlantisStructures {
     public static final RegistryObject<StructureType<AtlantisHouse3>> ATLANTIS_HOUSE_3 = DEFERRED_REGISTRY_STRUCTURE.register("atlantis_house_3", () -> () -> AtlantisHouse3.CODEC);
     public static final RegistryObject<StructureType<AtlantisTower>> ATLANTIS_TOWER = DEFERRED_REGISTRY_STRUCTURE.register("atlantis_tower", () -> () -> AtlantisTower.CODEC);
     public static final RegistryObject<StructureType<OysterStructure>> OYSTER_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("oyster_structure", () -> () -> OysterStructure.CODEC);
+    public static final RegistryObject<StructureType<AtlanteanTemple>> ATLANTEAN_TEMPLE = DEFERRED_REGISTRY_STRUCTURE.register("atlantean_temple", () -> () -> AtlanteanTemple.CODEC);
 
 }
