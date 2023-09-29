bf117bc0e23b610a9b9e52a58718bd3c99fe1172
Mysticpasta1 <ethana5tc@outlook.com>
2023-09-29 04:54:30 -0500

    sapling

--- src/main/java/com/mystic/atlantis/entities/renders/SubmarineEntityRenderer.java
+++ src/main/java/com/mystic/atlantis/entities/renders/SubmarineEntityRenderer.java
@@ -17,7 +17,7 @@ public class SubmarineEntityRenderer extends GeoEntityRenderer<SubmarineEntity> 
     public SubmarineEntityRenderer(EntityRendererProvider.Context ctx) {
         super(ctx, new SubmarineEntityModel());
     }
-    
+
     @Override
     public void preRender(PoseStack poseStack, SubmarineEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
