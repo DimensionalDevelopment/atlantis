15ead89f8b36c5f4d8e942a0eda2ee49ce0b120c
Waterpicker <waterpickerenternity@gmail.com>
2025-07-27 13:15:25 -0500

    Sounds

--- src/main/java/com/mystic/atlantis/init/AtlantisSoundEventInit.java
+++ src/main/java/com/mystic/atlantis/init/AtlantisSoundEventInit.java
@@ -1,25 +1,24 @@
 package com.mystic.atlantis.init;
 
+import com.mystic.atlantis.Atlantis;
 import com.mystic.atlantis.util.Reference;
-
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class AtlantisSoundEventInit {
-	public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Reference.MODID);
+	public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(Registries.SOUND_EVENT, Reference.MODID);
 
-    public static final RegistryObject<SoundEvent> PANBEE = registerSound("panbee");
-    public static final RegistryObject<SoundEvent> COLUMN = registerSound("column_cavitation");
+    public static final DeferredHolder<SoundEvent, SoundEvent> PANBEE = registerSound("panbee");
+    public static final DeferredHolder<SoundEvent, SoundEvent> COLUMN = registerSound("column_cavitation");
 
-    private static RegistryObject<SoundEvent> registerSound(String name) {
-        return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Reference.MODID, name)));
+    private static DeferredHolder<SoundEvent, SoundEvent> registerSound(String name) {
+        return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(Atlantis.id(name)));
     }
     
     public static void init(IEventBus bus) {
-    	SOUNDS.register(bus);;
+    	SOUNDS.register(bus);
     }
 }
